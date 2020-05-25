79111a28d03a2adb8c67d05cbc7d1cc226d25a25
Kegbi <kegbi.networking@gmail.com>
2020-05-25 23:33:49 +0300

    Updates

---
+++ precache-manifest.08ed70cad72e947f00849ea5021ca20b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9990c029bf51291a1c6269027d2692f0",
+    "url": "/RoboSearch/index.html"
+  },
+  {
+    "revision": "ff87b2d8267e996737b3",
+    "url": "/RoboSearch/static/css/main.580c6f4c.chunk.css"
+  },
+  {
+    "revision": "9944227db7fe65341146",
+    "url": "/RoboSearch/static/js/2.5b17ec29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboSearch/static/js/2.5b17ec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff87b2d8267e996737b3",
+    "url": "/RoboSearch/static/js/main.24e573b7.chunk.js"
+  },
+  {
+    "revision": "7b768c2ff4971a4277ae",
+    "url": "/RoboSearch/static/js/runtime-main.dd5cd028.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboSearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
