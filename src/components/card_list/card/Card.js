a6c398fa334a949e1ff0047857dc748089e6a09b
Kegbi <kegbi.networking@gmail.com>
2020-05-26 18:05:01 +0300

    Added some pwa magic to make lighthouse happy. Whoosh!

--- src/components/card_list/card/Card.js
+++ src/components/card_list/card/Card.js
@@ -4,7 +4,7 @@ import './card.scss';
 function Card({name, id, email}) {
   return(
     <div className={'card'}>
-      <img src={`https://robohash.org/${id}?set=set3`} alt="kitty"/>
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="kitty"/>
       <h2 className={'card__header'}>{name}</h2>
       <p className={'card_mail'}>{email}</p>
     </div>
